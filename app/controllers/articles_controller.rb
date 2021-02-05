1eeec40b190195715ee388c24f6ee8901a91229f
carlosfelipeg <carlosfelipe20082008@hotmail.com>
2021-02-05 15:36:53 -0500

    before action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,28 +1,36 @@
 class ArticlesController < ApplicationController
-    
+
+
+    before_action :find_article, except: [:new,:create]
+
     def show
-        @article = Article.find(params[:id])
     end
     
     def new
         @article = Article.new
     end
+    
     def create
         @article = Article.create(title: params[:article][:title], content: params[:article][:content])
         render json: @article
     end
+    
     def edit
-        @article = Article.find(params[:id])
     end
+    
     def update
-        @article = Article.find(params[:id])
+        
         @article = Article.update(title: params[:article][:title], content: params[:article][:content])
         redirect_to @article[0]
     end
+    
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         redirect_to root_path
     end
     
+    def find_article
+        @article = Article.find(params[:id])
+    end
+
 end
