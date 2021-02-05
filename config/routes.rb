e41dff39b6d95d0bbe3950942ba9001ae401ea35
carlosfelipeg <carlosfelipe20082008@hotmail.com>
2021-02-05 16:19:41 -0500

    adding nav

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
   get "bienvenida", to: "home#index"
   
   root to: "home#index"
-  
-  get "articles/new", to: "articles#new"
+
+  get "articles", to:"articles#index"
+  get "articles/new", to: "articles#new", as: :new_articles
   post "articles", to: "articles#create"
   get "articles/:id", to: "articles#show"
   get "articles/:id/edit", to: "articles#edit"
-  patch "articles/:id", to: "articles#update" , as: :article
+  patch "articles/:id", to: "articles#update", as: :article
   delete "articles/:id", to: "articles#destroy"
 end
