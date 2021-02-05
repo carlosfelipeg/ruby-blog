854604bc0e894a7a41dbb6f1592f6c5c8b2495db
carlosfelipeg <carlosfelipe20082008@hotmail.com>
2021-02-04 19:01:39 -0500

    show articles

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get "bienvenida", to: "home#index"
   get "articles/new", to: "articles#new"
   post "articles", to: "articles#create"
+  get "articles/:id", to: "articles#show"
 end
