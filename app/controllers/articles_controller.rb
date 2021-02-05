c67af102f18a7217fca27c1c3aef58352d389682
carlosfelipeg <carlosfelipe20082008@hotmail.com>
2021-02-05 17:16:45 -0500

    adding devise auth

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,7 @@ class ArticlesController < ApplicationController
 
 
     before_action :find_article, except: [:new,:create,:index]
+    before_action : authenticate_user, only [:new,:create,:edit,:destroy]
 
     def index
         @articles = Article.all
